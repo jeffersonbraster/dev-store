bbf7304e364ebba6c21421fa7e5debb037bddca6
Jefferson <jefferson.brandao@g.globo>
2023-10-25 22:26:12 -0300

    criando pagina de produto

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -22,7 +22,7 @@ export default async function Home() {
   return (
     <section className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
       <Link
-        href={`/products/${hilightedProduct.slug}`}
+        href={`/product/${hilightedProduct.slug}`}
         className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
       >
         <Image
@@ -48,7 +48,7 @@ export default async function Home() {
       {otherProducts.map((product) => (
         <Link
           key={product.id}
-          href={`/products/${product.slug}`}
+          href={`/product/${product.slug}`}
           className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
         >
           <Image
