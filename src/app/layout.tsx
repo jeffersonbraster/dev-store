36aac6769585ab355c50ae77340ee1a6504e5f75
Jefferson <jefferson.brandao@g.globo>
2023-10-25 21:05:50 -0300

    componente header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="pt-br">
-      <body className={inter.variable}>{children}</body>
+    <html lang="pt-br" className={inter.variable}>
+      <body className="bg-zinc-950 text-zinc-50 antialiased">{children}</body>
     </html>
   )
 }
