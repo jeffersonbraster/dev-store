6da65b2af4a7e8bea2b11f9b0e98fbaec66d9e85
Jefferson <jefferson.brandao@g.globo>
2023-10-25 23:53:52 -0300

    buscando o produto

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,11 +1,34 @@
+import { api } from '@/app/data/api'
+import { Product } from '@/app/data/types/product'
 import Image from 'next/image'
 
-export default function ProductPage() {
+interface ProductProps {
+  params: {
+    slug: string
+  }
+}
+
+async function getProduct(slug: string): Promise<Product> {
+  const response = await api(`/products/${slug}`, {
+    // cache: 'no-store',
+    next: {
+      revalidate: 60 * 60 * 24, // 24 hours
+    },
+  })
+
+  const product = await response.json()
+
+  return product
+}
+
+export default async function ProductPage({ params }: ProductProps) {
+  const product = await getProduct(params.slug)
+
   return (
     <div className="relative grid max-h-[860px] grid-cols-3">
       <div className="col-span-2 overflow-hidden">
         <Image
-          src="/camiseta-dowhile-2022.png"
+          src={product.image}
           alt=""
           width={1000}
           height={1000}
@@ -15,15 +38,26 @@ export default function ProductPage() {
       </div>
 
       <div className="flex flex-col justify-center px-12">
-        <h1 className="text-3xl font-bold leading-tight">Moletom never stop</h1>
+        <h1 className="text-3xl font-bold leading-tight">{product.title}</h1>
 
-        <p className="mt-2 leading-relaxed text-zinc-400">um teste</p>
+        <p className="mt-2 leading-relaxed text-zinc-400">
+          {product.description}
+        </p>
 
         <div className="mt-8 flex items-center gap-3">
           <span className="inline-block px-5 py-2.5 rounded-full bg-violet-500 font-semibold">
-            129
+            {product.price.toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+            })}
           </span>
-          <span className="text-sm text-zinc-400">Em 12x s/ juros de 10</span>
+          <span className="text-sm text-zinc-400">
+            ou em até 12x de{' '}
+            {(product.price / 12).toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+            })}
+          </span>
         </div>
 
         <div className="mt-8 space-y-4">
