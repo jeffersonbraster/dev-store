34258482ae6e9558809934c2a51e81ec1308f588
Jefferson <jefferson.brandao@g.globo>
2023-10-29 23:00:32 -0300

    gerando opengraph image

---
+++ src/app/(store)/product/[slug]/opengraph-image.tsx
@@ -0,0 +1,57 @@
+/* eslint-disable @next/next/no-img-element */
+import { ImageResponse } from 'next/server'
+import colors from 'tailwindcss/colors'
+
+import { api } from '@/app/data/api'
+import { env } from '@/app/env'
+import { Product } from '@/app/data/types/product'
+
+export const runtime = 'edge'
+
+export const alt = ''
+
+export const size = {
+  width: 1200,
+  height: 630,
+}
+
+export const contentType = 'image/png'
+
+async function getProduct(slug: string): Promise<Product> {
+  const response = await api(`/products/${slug}`, {
+    cache: 'force-cache',
+  })
+
+  const product = await response.json()
+
+  return product
+}
+
+export default async function OgImage({
+  params,
+}: {
+  params: { slug: string }
+}) {
+  const product = await getProduct(params.slug)
+
+  const productImageURL = new URL(product.image, env.APP_URL).toString()
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          background: colors.zinc[950],
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <img src={productImageURL} alt="" style={{ width: '100%' }} />
+      </div>
+    ),
+    {
+      ...size,
+    },
+  )
+}
